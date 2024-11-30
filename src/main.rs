25e208d606f085b481c134935decb0df78d0fe54
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-11-29 20:36:02 -0500

    multi parameter functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    another_function(5);
+    another_function(5, 'h');
 }
 
-fn another_function(x: u32) {
-    println!("Hello, world from another function with passed in value {x}!");
+fn another_function(x: u32, y: char) {
+    println!("Hello, world from another function with passed in value {x}{y}!");
 }
