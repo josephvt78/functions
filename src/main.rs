ddb13ee654169a109497abdb81327913c64c6f5c
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-11-29 20:34:17 -0500

    function with parameters

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    another_function();
+    another_function(5);
 }
 
-fn another_function() {
-    println!("Hello, world from another function!");
+fn another_function(x: u32) {
+    println!("Hello, world from another function with passed in value {x}!");
 }
